c7888808f36c5d6bd24eaeea7dfc88777fcaafe9
Lawrence Gunn <lawrencegunn@gmail.com>
2015-03-27 14:28:27 -0700

    Args.

--- Development/public/two-neuron.js
+++ Development/public/two-neuron.js
@@ -3,10 +3,12 @@ N.Template(
   function(root, self, imports, compiler) {
     var config = {
       "className": 'N.Network',
-      "name": "Hello World"//,
-      //"build": imports["add-neurons"]()
+      "name": "Hello World",
+      "build": [ { id: "add-neurons", args: [] } ]
     };
 
     _.merge(self, config);
+
+    compiler.buildOut(root, self, imports);
   }
 );
