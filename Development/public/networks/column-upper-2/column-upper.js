f5b9b59805febcac2a0f5a78ae7429da03b500b7
Lawrence Gunn <lawrencegunn@gmail.com>
2015-06-17 10:17:46 -0700

    Rewrite of display attribute.

--- Development/public/networks/column-upper-2/column-upper.js
+++ Development/public/networks/column-upper-2/column-upper.js
@@ -2,11 +2,13 @@
 N.Mod.Column = function(context) {
 
   var getDependencies = function() {
-    return ['/networks/column-upper-2/layer-1.js'];
+    return [
+      '/networks/column-upper-2/layer-1.js'];
   };
 
   var create = function(name) {
     var column = context.createEmptyNetwork(name || 'COL');
+    column.displaySource = '/networks/column-upper-1/column-upper.display.json';
     var l1 = context.makeModule("N.Mod.Layer1");
     column.networks.push(l1.create());
     return column;
